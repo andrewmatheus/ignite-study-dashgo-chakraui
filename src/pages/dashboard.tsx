bf6b1695f5be644d6f8a1193b205de84a35c1750
Andrew Matheus <andrewmatheus@gmail.com>
2021-07-05 23:56:10 -0300

    fix

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -25,6 +25,36 @@ const options = {
   },
   dataLabels: {
     enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000Z',
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+      '2021-03-24T00:00:00.000Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7, 
+      opacityTo: 0.3, 
+    }
   }
 }
 
@@ -54,7 +84,7 @@ export default function Dashboard() {
             p="8"
             bg="gray.800"
             borderRadius={8}
-            // pb="4"
+            pb="4"
           >
             <Text fontSize="lg" mb="4">Inscritos da semana</Text>
             <Chart options={options} series={series} type="area" height={160} />
@@ -64,9 +94,10 @@ export default function Dashboard() {
             p="8"
             bg="gray.800"
             borderRadius={8}
-            // pb="4"
+            pb="4"
           >
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid> 
       </Flex>
